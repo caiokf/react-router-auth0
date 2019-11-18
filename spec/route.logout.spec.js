3b44737f0b3d1de65a17a84502aae7ff34f45850
Caio Kinzel Filho <caiokf@gmail.com>
2019-11-18 15:53:21 +0200

    specs for logout route

---
+++ spec/route.logout.spec.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { Redirect } from 'react-router-dom'
+
+import LogoutRoute from '../src/route.logout'
+
+describe('Login Route', () => {
+  let auth0MockResult
+
+  beforeEach(() => {
+    auth0MockResult = {
+      logout: jest.fn(),
+    }
+
+    window.ReactRouterAuth0Provider = auth0MockResult
+  })
+
+  it('shoul call logout when rendered', () => {
+    shallow(<LogoutRoute />)
+
+    expect(auth0MockResult.logout).toHaveBeenCalled()
+  })
+
+  it('redirects when rendered', () => {
+    window.ReactRouterAuth0Provider.isLoggedIn = () => true
+
+    const wrapper = shallow(<LogoutRoute />)
+    expect(wrapper.find(Redirect)).toHaveLength(1)
+  })
+})
