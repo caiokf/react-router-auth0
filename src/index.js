ed584a9c95c8c54d1397f16a3a14b50db600b636
Caio Kinzel Filho <caiokf@gmail.com>
2019-11-18 11:40:35 +0200

    initial commit: bringing src over to new repo

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import PrivateRoute from './route.private'
+import LoginRoute from './route.login'
+import LogoutRoute from './route.logout'
+import LandingPageRoute from './route.landing'
+import AuthCallbackRoute from './route.callback'
+
+export {
+    PrivateRoute, 
+    LoginRoute,
+    LogoutRoute,
+    LandingPageRoute,
+    AuthCallbackRoute,
+}
